eeea3acfc4e70d40c5b543788edc770cf77a0fb1
Giovanni Blanco <Giovanni.Blanco@jala.university>
2023-03-14 12:25:30 -0400

    Add initial documentation

--- src/converters/pdfConvert.js
+++ src/converters/pdfConvert.js
@@ -1,27 +1,31 @@
+/* Importing the dotenv module, the pdfCommand module and the Execute module. */
 const dotenv = require('dotenv');
 const { pdfCommand } = require('./pdfConverter/pdfCommand.js');
 const { Execute } = require('./Execute.js');
 
+/* Loading the environment variables from the .env file. */
 dotenv.config({path: __dirname + '/../../.env'});
 
+/* Just defining the input and output file names. */
 const newFile = 'test.pdf';
 const outExtension = 'jpg';
-//Creates a new object audio for image commands
+/* Creating a new instance of the pdfCommand class. */
 let pdf = new pdfCommand();
-//Creates an object for executing the commands that were sent
+/* Creating a new instance of the Execute class. */
 let execute = new Execute();
 //Adds the input file with its address to convert
 pdf.inputFile = `${process.env.UPLOADS_PATH_PDF}${newFile}`;
 //Adds the extension of the images output files
 pdf.outExtension = outExtension;
-//Sets the parameters of convertion
+/* Setting the density and quality of the output images. */
 pdf.newDensity = 150;
 pdf.newQuality = 90;
-//Creates the output path according to design
+/* Creating the output path for the converted file. */
 const outFilePath = `${process.env.DOWNLOAD_PATH_PDF}${pdf.fileName(newFile)}.${outExtension}`;
 pdf.convertedFilePath = outFilePath;
-//Gets the command to execute the desired action
+/* Getting the command to execute the desired action. */
 let command = pdf.getCommand();
+/* Printing the command to the console. */
 console.log(command);
 //Converts the input file and returns the state of the conversion
 execute.command(command, pdf.convertedFilePath);
--- src/converters/pdfConverter/pdfCommand.js
+++ src/converters/pdfConverter/pdfCommand.js
@@ -5,6 +5,9 @@ class pdfCommand extends Command {
 
     #quality;
 
+    /**
+     * The constructor function is used to initialize the object's properties.
+     */
     constructor() {
         super();
         //this.#bitRate='';
@@ -15,14 +18,29 @@ class pdfCommand extends Command {
     }
 
 
+    /**
+     * The function takes a density value as an argument and sets the density property of the class to
+     * the value of the argument
+     * @param densityValue - The density value to set.
+     */
     set newDensity(densityValue) {
         this.#density = `-density ${densityValue}`;
     }
 
+    /**
+     * The function takes a quality value and sets the quality variable to the quality value.
+     * @param qualityValue - The quality value you want to set.
+     */
     set newQuality(qualityValue) {
         this.#quality = `-quality ${qualityValue}`;
     }
 
+    /**
+     * "The function gets the command to convert the file from the super class and then adds the
+     * density and quality to the command."
+     * </code>
+     * @returns The command to be executed.
+     */
     getCommand() {
         const converter = process.env.MAGICK;
         var command = `${converter} ${this.#density} ${super.inputFile} ${this.#quality} ${super.convertedFilePath}`;
@@ -30,6 +48,7 @@ class pdfCommand extends Command {
     }
 }
 
+/* Exporting the pdfCommand class to be used in other files. */
 module.exports = {
     pdfCommand,
 };
