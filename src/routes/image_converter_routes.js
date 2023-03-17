fefbb2895992e52e856b9a73d281448bd4080c7c
Percy Fernandez <Percy.Fernandez@jala.university>
2023-03-17 10:45:42 -0400

    changes on documentation on routes middlewares routes

--- src/routes/image_converter_routes.js
+++ src/routes/image_converter_routes.js
@@ -1,6 +1,21 @@
+/*
+* @image_converter_routes.js Copyright(c) 2023 Jalasoft
+* 2643 Av Melchor Perez de Olguin, Colquiri Sud, Cochabamba, Bolivia.
+* Av.General Inofuentes esquina Calle20, Edificio Union No1376, La Paz, Bolivia
+* All rights reserved
+* This software is the confidential and proprietary information of
+* Jalasoft,ConfidentialInformation"). You shall not
+* disclose such Confidential Information and shall use it only in
+* accordance with the terms of the license agreement you entered into
+* with Jalasoft
+*/
+
+
 const express = require('express');
 const router = express.Router();
+//Importing the ImageConverterController class from the image_converter_controller.js file.
 const ImageConverterController = require('../controllers/image_converter_controller');
+//Importing the uploadImage function from the converter_middleware.js file.
 const { uploadImage } = require('../middlewares/converter_middleware.js');
 
 const converter = new ImageConverterController();
